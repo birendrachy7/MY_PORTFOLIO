638f580561c68c247cb9d5f2b4bdb5cfe009f00e
birendrachy7 <er.birendra.chy7@gmail.com>
2026-03-04 12:50:32 +0545

    Portfolio

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,12 +1,26 @@
+import Navbar from "./components/Navbar"
+import ParticlesBackground from "./components/ParticlesBackground"
+import About from "./sections/About"
+import Certificates from "./sections/Certificates"
+import Contact from "./sections/Contact"
+import Footer from "./sections/Footer"
+import Home from "./sections/Home"
+import Projects from "./sections/Projects"
+import Skills from "./sections/Skills"
 
-
-function App() {
-  
+export default function App() {
   return (
-    <>
-      
-    </>
+    <div className="relative gradient text-white">
+    <ParticlesBackground/>
+    <Navbar/>
+    <Home/>
+    <About/>
+    <Skills/>
+    <Projects/>
+    <Certificates/>
+    <Contact/>
+    <Footer/>
+    </div>
+
   )
 }
-
-export default App
---
+++ my-portfolio/src/sections/Certificates.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Certificates = () => {
+  return (
+    <div className='w-full h-screen'>
+      Certificates
+    </div>
+  )
+}
+
+export default Certificates
