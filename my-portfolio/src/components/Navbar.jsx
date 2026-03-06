ffd8069124b6af2fc4aac287a7166b7f7fee2711
birendrachy7 <er.birendra.chy7@gmail.com>
2026-03-06 11:36:25 +0545

    Commit

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -1,102 +1,30 @@
-import { useEffect, useRef, useState } from "react";
-import OverlayMenu from "./OverlayMenu";
-import logo from "../assets/logo.png";
-import { FiMenu } from "react-icons/fi";
+import CustomCursor from "./components/CustomCursor"
+import Navbar from "./components/Navbar"
+//import ParticlesBackground from "./components/ParticlesBackground"
+import About from "./sections/About"
+import Certificates from "./sections/Certificates"
+import Contact from "./sections/Contact"
+import Education from "./sections/Education"
+import Footer from "./sections/Footer"
+import Home from "./sections/Home"
+import Projects from "./sections/Projects"
+import Skills from "./sections/Skills"
 
-export default function Navbar() {
-
-    const [menuOpen, setMenuOpen] = useState(false);
-    const [visible, setVisible] = useState(true);
-    const [forceVisible, setForceVisible] = useState(false);
-
-    const lastScrollY = useRef(0);
-    const timerId = useRef(null);
-
-    useEffect(() => {
-        const homeSection = document.querySelector("#home");
-        const observer = new IntersectionObserver(
-            ([entry]) => {
-                if(entry.isIntersecting){
-                    setForceVisible(true);
-                    setVisible(true);   
-                }else{
-                    setForceVisible(false);
-                }
-            },{threshold: 0.1}
-        )
-
-        if(homeSection) observer.observe(homeSection);
-        return()=>{
-            if(homeSection) observer.unobserve(homeSection);
-        }
-    },[])
-
-    useEffect(() => {
-            const handleScroll = () => {
-            if(forceVisible){
-                setVisible(true);
-                return;
-            }
-        const currentScrollY = window.scrollY;
-        if(currentScrollY> lastScrollY.current){
-            setVisible(false);
-        } else {
-            setVisible(true);
-
-            if(timerId.current) clearTimeout(timerId.current);
-            timerId.current = setTimeout(() => {
-                setVisible(false);
-            }, 300);
-        }
-lastScrollY.current = currentScrollY;
-
-     }
-    window.addEventListener("scroll", handleScroll, {passive: true});
-return () => {
-    window.removeEventListener("scroll", handleScroll);
-    if(timerId.current) clearTimeout(timerId.current);
-}
-    },[forceVisible])
-
-
-
-return (
-<>
-<nav className={`fixed top-0 left-0 w-full flex items-center justify-between px-6 py-4 z-50 transition-transform duration-300 ${visible ? "translate-y-0" : "-translate-y-full"}`}>
-
-<div className="flex items-center space-x-2">
-<img src={logo} alt="logo" className="w-10 h-10"/>  
-<div className="text-2xl font-bold text-white hidden sm:block">
-    BIRU
+export default function App() {
+  return (
+    <div className="relative gradient text-white">
+      <CustomCursor/> 
+    {/*<ParticlesBackground/>*/}
+    <Navbar/>
+    <Home/>
+    <About/>
+    <Education/>
+    <Skills/>
+    <Projects/>
+    <Certificates/>
+    <Contact/>
+    <Footer/>
     </div>
-</div>
 
-<div className="block lg:absolute lg:left-1/2 lg:-translate-x-1/2">
-<button onClick={()=>setMenuOpen(true)}
-    
-    className="text-white text-3xl focus:outline-none"
-    aria-label="open Menu">
-
-    <FiMenu />
-</button>
-
-</div>
-
-<div className="hidden lg:block">
-    <a href="#contact"
-    className="bg-gradient-to-r from-pink-500 to-purple-500 text-white px-5 py-2 rounded-full font-medium shadow-lg hover:opacity-90 transition-opacity duration-300"
-    >
-    Contact me
-    </a>
-
-</div>
-
-</nav>
-
-<OverlayMenu isOpen= {menuOpen} onClose={()=>setMenuOpen(false)}/>
-</>
-
-)
-
-
+  )
 }
